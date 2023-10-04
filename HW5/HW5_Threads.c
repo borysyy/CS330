fdd95ce29a0aad9ce3666d4a8e6d46529157c107
Slava Borysyuk <borysyy@sunypoly.edu>
2023-10-04 13:35:20 -0400

    HW5 folder

---
+++ HW5/HW5_Threads.c
@@ -0,0 +1,11 @@
+/*
+Written by: Slava Borysyuk
+Course: CS330
+10/4/2023
+HW4_Process_Affinity
+Output:
+Single processor performance -
+Elapsed Time: 0 seconds and 15970 microseconds
+Multiprocessor performance -
+Elapsed Time: 0 seconds and 8018 microseconds
+*/
